e6b46493f75cbad65e8dbfb738d00e49131375c6
Tharun Balaji R <95350584+TharunBalaji2004@users.noreply.github.com>
2023-03-18 13:13:29 +0530

    Added Source Code

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var options = {
+    strings: ["Tharun Balaji","App Developer","Web Developer","ML Enthusiast"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+};
+
+var typed = new Typed(".typing",options);
